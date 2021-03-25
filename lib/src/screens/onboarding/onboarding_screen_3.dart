2fac9e57f48e348b3a2427bf58a058c09da826fb
Abhishek <abhishek0saini@gmail.com>
2021-03-26 00:46:36 +0530

    add taskDescription widget

--- lib/src/screens/onboarding/onboarding_screen_3.dart
+++ lib/src/screens/onboarding/onboarding_screen_3.dart
@@ -21,8 +21,10 @@ class OnboardingScreen3 extends StatelessWidget {
               header: 'What brings you to ',
               highlightedText: 'duuit!',
             ),
-            Padding(padding: EdgeInsets.only(top: 40)),
-            selectedCategory(args.selectedCategory)
+            Padding(padding: EdgeInsets.only(top: 20)),
+            selectedCategory(args.selectedCategory), 
+            Padding(padding: EdgeInsets.only(top: 5)),
+            taskDescription(),
           ],
         ),
       ),
@@ -40,4 +42,56 @@ class OnboardingScreen3 extends StatelessWidget {
       ),
     );
   }
+
+  Widget taskDescription() {
+    return Stack(
+        children: <Widget> [ 
+        Container(
+          height: 100,
+          margin: EdgeInsets.only(left: 55, right: 55),
+          padding: EdgeInsets.only(top: 10, bottom: 10),
+
+          child: TextField(
+            minLines: 10,
+            maxLines: 10,
+            decoration: textFieldDecoration(hintText: 'I would like to achieve'),
+          ),
+        ),
+        Positioned(
+          left: 60,
+          top: 2,
+          child: Container(
+            padding: EdgeInsets.only(bottom: 5, left: 2, right: 2),
+            color: Colors.white,
+            child: Text(
+              'Description',
+              style: TextStyle(color: Colors.black, fontSize: 14),
+            ),
+          )
+        ),
+      ]
+    );
+  }
+
+  InputDecoration textFieldDecoration({String hintText}) {
+    return InputDecoration(
+        contentPadding: EdgeInsets.all(10),
+        focusedBorder: textFieldBorder(),
+        enabledBorder: textFieldBorder(),
+        hintText: hintText,
+        hintStyle: TextStyle(
+          fontSize: 14.0,
+          fontWeight: FontWeight.w400,
+        )
+    );
+  }
+
+  OutlineInputBorder textFieldBorder() {
+    return OutlineInputBorder(
+        borderRadius: BorderRadius.circular(10),
+        borderSide: BorderSide(color: Colors.black45)
+    );
+  }
+
 }
+
