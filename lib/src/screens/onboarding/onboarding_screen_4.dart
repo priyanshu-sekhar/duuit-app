f185fb779f4e3de14010daa42e238ba2f364d2f4
Priyanshu Sekhar <pspatra@amazon.com>
2021-03-27 03:18:22 +0000

    mock list with list item design for OnboardingScreen4

--- lib/src/screens/onboarding/onboarding_screen_4.dart
+++ lib/src/screens/onboarding/onboarding_screen_4.dart
@@ -1,13 +1,18 @@
+import 'package:duuit/src/screens/tiles/buddy_tile.dart';
 import 'package:duuit/src/widgets/continue_button.dart';
 import 'package:duuit/src/widgets/header.dart';
 import 'package:duuit/src/widgets/onboarding_header.dart';
-import 'package:expandable/expandable.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class OnboardingScreen4 extends StatelessWidget {
+class OnboardingScreen4 extends StatefulWidget {
   static const route = '/onboarding/4';
 
+  @override
+  OnboardingScreen4State createState() => OnboardingScreen4State();
+}
+
+class OnboardingScreen4State extends State<OnboardingScreen4> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,7 +26,10 @@ class OnboardingScreen4 extends StatelessWidget {
             ),
             subtitle(),
             Padding(padding: EdgeInsets.only(top: 20)),
-            buddyList(),
+            Container(
+              padding: EdgeInsets.only(left: 20, right: 20),
+              child: buddyList(),
+            ),
             Spacer(),
             Padding(padding: EdgeInsets.only(top: 24)),
             ContinueButton(),
@@ -49,73 +57,8 @@ class OnboardingScreen4 extends StatelessWidget {
   }
 
   Widget buddyList() {
-    return buddyTile();
-  }
-
-  Widget buddyTile() {
-    return Container(
-      margin: EdgeInsets.only(left: 36, right: 36, bottom: 18),
-      decoration: BoxDecoration(
-          color: Colors.white,
-          boxShadow: [
-            BoxShadow(
-                offset: Offset(3, 5),
-                color: Color.fromRGBO(6, 23, 44, 0.09),
-                blurRadius: 5),
-          ],
-          borderRadius: BorderRadius.circular(20)),
-      child: ExpandablePanel(
-        header: Container(
-          padding: EdgeInsets.only(top: 10, left: 7, right: 10),
-          height: 82,
-          child: Container(
-            child: Row(
-              // crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Image.asset(
-                  'assets/profile_pic.png',
-                  height: 60,
-                ),
-                Padding(padding: EdgeInsets.only(left: 5)),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [username(), goal()],
-                )
-              ],
-            ),
-          ),
-          // decoration: BoxDecoration(color: Colors.red),
-        ),
-        collapsed: Container(
-          height: 0,
-        ),
-        expanded: Text(
-          'article.body',
-          softWrap: true,
-        ),
-      ),
-    );
-  }
-
-  Text goal() {
-    return Text(
-      'Workout',
-      style: TextStyle(
-          color: Color.fromRGBO(0, 0, 0, 0.5),
-          fontSize: 14,
-          fontWeight: FontWeight.w400),
-    );
-  }
-
-  Text username() {
-    return Text(
-      'Siddharth Dash',
-      style: TextStyle(
-          fontWeight: FontWeight.w600,
-          fontSize: 17,
-          color: Color(0xFF06172C),
-          height: 2),
-    );
+    return ListView(shrinkWrap: true, children: [
+      BuddyTile()
+    ]);
   }
 }
---
+++ lib/src/screens/tiles/buddy_tile.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class BuddyTile extends StatelessWidget {
+  final String profilePic;
+  final String userName;
+  final String description;
+  final String goal;
+  final String goalDuration;
+
+  BuddyTile(
+      {this.profilePic = 'assets/profile_pic.png',
+      this.userName = 'Siddharth Dash',
+      this.description =
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nisi tellus facilisis suspendisse adipiscing ac. In porttitor fusce imperdiet feugiat a volutpat.',
+      this.goal = 'Workout',
+      this.goalDuration = 'For 2 weeks'});
+
+  @override
+  Widget build(BuildContext context) {
+    return PhysicalModel(
+      borderRadius: BorderRadius.circular(10),
+      child: ClipRRect(
+        child: Material(
+          color: Colors.white,
+          child: itemTile(),
+        ),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      elevation: 4,
+      color: Color.fromRGBO(6, 23, 44, 0.09),
+    );
+  }
+
+  Widget itemTile() {
+    return ExpansionTile(
+      leading: Transform.translate(
+        offset: Offset(0, 10),
+        child: Image.asset(
+          profilePic,
+          height: 80,
+        ),
+      ),
+      title: transformChild(child: username()),
+      subtitle: transformChild(child: userSubtitle()),
+      tilePadding: EdgeInsets.only(left: 10, right: 10, bottom: 10),
+      children: [
+        Transform.translate(
+          offset: Offset(0, -15),
+          child: Column(
+            children: [
+              Divider(color: Color.fromRGBO(0, 0, 0, 0.5)),
+              Padding(padding: EdgeInsets.only(top: 8)),
+              alignText(text: userName),
+              Padding(padding: EdgeInsets.only(top: 4)),
+              alignText(text: description)
+            ],
+          ),
+        ),
+      ],
+      childrenPadding: EdgeInsets.only(left: 20, right: 20, top: 20),
+    );
+  }
+
+  Transform transformChild({child}) {
+    return Transform.translate(
+      offset: Offset(-10, 10),
+      child: child,
+    );
+  }
+
+  Align alignText({text}) {
+    return Align(
+      alignment: Alignment.topLeft,
+      child: Text(
+        text,
+        style: subtextStyle(),
+      ),
+    );
+  }
+
+  Row userSubtitle() {
+    return Row(
+      children: [
+        userGoal(),
+        Spacer(),
+        Transform.translate(
+          offset: Offset(50, 0),
+          child: Text(
+            goalDuration,
+            style: TextStyle(
+              fontWeight: FontWeight.w400,
+              fontSize: 14,
+              color: Color.fromRGBO(0, 0, 0, 0.5),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Text userGoal() {
+    return Text(
+      goal,
+      style: subtextStyle(),
+    );
+  }
+
+  Text username() {
+    return Text(
+      userName,
+      style: TextStyle(
+        fontWeight: FontWeight.w600,
+        fontSize: 17,
+        color: Color(0xFF06172C),
+        height: 1,
+      ),
+    );
+  }
+
+  TextStyle subtextStyle() {
+    return TextStyle(
+      fontWeight: FontWeight.w400,
+      fontSize: 14,
+      color: Color.fromRGBO(0, 0, 0, 0.5),
+    );
+  }
+}
