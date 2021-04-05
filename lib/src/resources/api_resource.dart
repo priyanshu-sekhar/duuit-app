ef286930123157f877f4043da9a00956235dbabb
Priyanshu Sekhar <pspatra@amazon.com>
2021-04-05 22:34:15 +0100

    changed root to https api

--- lib/src/resources/api_resource.dart
+++ lib/src/resources/api_resource.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'package:duuit/src/args/user_args.dart';
 import 'package:duuit/src/models/access_token.dart';
 import 'package:duuit/src/models/request/add_goal_request.dart';
 import 'package:duuit/src/models/request/add_user_request.dart';
@@ -7,15 +6,15 @@ import 'package:duuit/src/models/response/user_details_response.dart';
 import 'package:duuit/src/resources/resource.dart';
 import 'package:http/http.dart';
 
-// final _root = 'duuit.io/v2/';
-final _root = 'localhost:5000';
+final _root = 'www.duuit.io';
+// final _root = 'localhost:5000';
 
 class ApiResource implements Source {
   Client client = Client();
 
   addUser(AddUserRequest request) async {
     final response = await client.post(
-      Uri.http(_root, '/v2/user'),
+      Uri.https(_root, '/v2/user'),
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
       },
@@ -25,7 +24,7 @@ class ApiResource implements Source {
 
   addGoal(AddGoalRequest request) async {
     await client.post(
-      Uri.http(_root, '/v2/goal'),
+      Uri.https(_root, '/v2/goal'),
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
       },
@@ -35,7 +34,7 @@ class ApiResource implements Source {
 
   Future<UserDetailsResponse> fetchUserDetails(AccessToken accessToken) async {
     final response = await client.get(
-      Uri.http(_root, '/v2/user/${accessToken.userId}'),
+      Uri.https(_root, '/v2/user/${accessToken.userId}'),
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
       }
@@ -46,7 +45,7 @@ class ApiResource implements Source {
 
   Future<List<UserDetailsResponse>> fetchBuddies() async {
     final response = await client.get(
-      Uri.http(_root, '/v2/user/all'),
+      Uri.https(_root, '/v2/user/all'),
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
       },
